1ff50307405c3ecc802271a293e9db923333cefe
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2024-08-05 18:17:23 +0700

    update render gio hang Quyen

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,36 @@
-
 var DSSP = [];
 
 //get API
-var urlApi = "https://66a7894253c13f22a3d01bc8.mockapi.io/mydau848/api/v1/Capstone";
-
+var urlApi =
+  "https://66a7894253c13f22a3d01bc8.mockapi.io/mydau848/api/v1/Capstone";
 
 function fetchListSP() {
-    axios({
-        url: urlApi,
-        method: "GET",
-    }).then(function(res){
-        // thành công
-        console.log(res.data)
-    }).catch(function(err){
-        //thất bại
-        console.log(err)
+  axios({
+    url: urlApi,
+    method: "GET",
+  })
+    .then(function (res) {
+      // thành công
+      renderSP(res.data);
+    })
+    .catch(function (err) {
+      //thất bại
+      console.log(err);
     });
 }
-fetchListSP()
+fetchListSP();
+
+function renderSP(list) {
+  var contentHTML = "";
+  for (var i = 0; i < list.length; i++) {
+    var stringSP = `<tr>
+    <td class="first-td"><img width="100" src="${list[i].img}" alt="">${list[i].name}</td>
+    <td>${list[i].price}</td>
+    <td>1</td>
+    <td>${list[i].price}</td>
+    <td><button onclick="" style="cursor: pointer;"><i class="fa fa-trash-alt"></i></button></td>
+    </tr>`;
+    contentHTML += stringSP;
+  }
+  document.getElementById("tblBody").innerHTML = contentHTML;
+}
